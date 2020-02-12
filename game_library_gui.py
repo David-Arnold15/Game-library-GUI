14698999bb8af4ea5ef83d0a618abf0c6e8822c9
David-Arnold15 <you@example.com>
2020-02-12 13:39:13 -0600

    modified:   game_library_gui.py

--- game_library_gui.py
+++ game_library_gui.py
@@ -58,57 +58,78 @@ class RemoveChooser(tk.Frame):
 """-------------------------------------------------------------------------------------------------------------------------------------"""
 class InfoFrameEntries(tk.Frame):
 
-    def __init__(self,master):
-        tk.Frame.__init__(self)    
+    def __init__(self,parent):
+        tk.Frame.__init__(self, master= parent)    
         #--------------------------------------
-        self.lbl_genre = tk.Label(text="Genre").grid(row="0", column="0")
-        self.ent_genre = tk.Entry(text="Genre")
+        self.lbl_genre = tk.Label(self, text="Genre").grid(row="0", column="0")
+        self.ent_genre = tk.Entry(self, text="Genre")
         self.ent_genre.grid(row="0", column="1")
         #---------------------------------------
-        self.lbl_title = tk.Label(text="Title").grid(row="0", column="2")
-        self.ent_title = tk.Entry(text="Title")
+        self.lbl_title = tk.Label(self, text="Title").grid(row="0", column="2")
+        self.ent_title = tk.Entry(self, text="Title")
         self.ent_title.grid(row="0", column="3")
         #---------------------------------------
-        self.lbl_dev = tk.Label(text="Developer").grid(row="1", column="0")
-        self.ent_dev = tk.Entry(text="Developer")
+        self.lbl_dev = tk.Label(self, text="Developer").grid(row="1", column="0")
+        self.ent_dev = tk.Entry(self, text="Developer")
         self.ent_dev.grid(row="1", column="1")    
         #---------------------------------------
-        self.lbl_pub = tk.Label(text="Publisher").grid(row="1", column="2")
-        self.ent_pub = tk.Entry(text="Publisher")
+        self.lbl_pub = tk.Label(self, text="Publisher").grid(row="1", column="2")
+        self.ent_pub = tk.Entry(self, text="Publisher")
         self.ent_pub.grid(row="1", column="3") 
         #---------------------------------------
-        self.lbl_system = tk.Label(text="System").grid(row="2", column="0")
-        self.ent_system = tk.Entry(text="System")
+        self.lbl_system = tk.Label(self, text="System").grid(row="2", column="0")
+        self.ent_system = tk.Entry(self, text="System")
         self.ent_system.grid(row="2", column="1")   
         #---------------------------------------
-        self.lbl_release_date = tk.Label(text="Release Date").grid(row="2", column="2")
-        self.ent_release_date = tk.Entry(text="Release Date")
+        self.lbl_release_date = tk.Label(self, text="Release Date").grid(row="2", column="2")
+        self.ent_release_date = tk.Entry(self, text="Release Date")
         self.ent_release_date.grid(row="2", column="3")
         #---------------------------------------
-        self.lbl_price = tk.Label(text="Price").grid(row="3", column="0")
-        self.ent_price = tk.Entry(text="Price")
+        self.lbl_price = tk.Label(self, text="Price").grid(row="3", column="0")
+        self.ent_price = tk.Entry(self, text="Price")
         self.ent_price.grid(row="3", column="1")
         #---------------------------------------
-        self.lbl_purchase_date = tk.Label(text="Purchase_date").grid(row="3", column="2")
-        self.ent_purchase_date = tk.Entry(text="Purchase Date")
-        self.ent_purchase_date.grid(row="3", column="3")        
+        self.lbl_purchase_date = tk.Label(self, text="Purchase_date").grid(row="3", column="2")
+        self.ent_purchase_date = tk.Entry(self, text="Purchase Date")
+        self.ent_purchase_date.grid(row="3", column="3")      
+        #--------------------------------------
+        self.lbl_rating = tk.Label(self, text="Rating").grid(row="4", column="0")
+        self.ent_rating = tk.Entry(self, text="Rating")
+        self.ent_rating.grid(row="4", column="1")        
+"""-------------------------------------------------------------------------------------------------------------------------------------"""
 class InfoFrame(tk.Frame):
     def __init__(self):
         tk.Frame.__init__(self)
         #-----------------------------------------
         self.info_frame_entries = InfoFrameEntries(self)
-        self.info_frame_entries.grid(row="0", column="0" ,columnspan="4")
-        #-----------------------------------------
-        self.ent_beat = tk.Entry()
-        self.ent_beat.grid(row="1", column="0")
-        self.lbl_beat = tk.Label(text="Beat it?")
-        self.lbl_beat.grid(row="1", column="1")
-        #-----------------------------------------
-        self.lbl_players = tk.Label(text="No. of Players")
-        self.lbl_players.grid(row="1", column="2")
-        self.ent_players = tk.Entry()
-        self.ent_players.grid(row="1", column="3")        
-        
+        self.info_frame_entries.grid(row="0", column="0" ,columnspan="3", sticky="news")
+        var1=None
+        chk_beat = tk.Checkbutton(self, text='Beat it?',variable=var1, onvalue=1, offvalue=0,)
+        chk_beat.grid(row="1", column="0")        
+        #---------------------------------------
+        options=["Single or Multiplayer?", "Single", "Multi"]
+        tkvar = tk.StringVar(self)
+        tkvar.set(options[0])
+        self.menu = tk.OptionMenu(self, tkvar, *options)
+        self.menu.grid(row="1", column="2", )
+        #------------------------------------------
+        self.lbl_notes = tk.Label(self, text="Notes: ").grid(row="2", column="0", sticky="w" )
+        self.scr_notes = scrolledtext.ScrolledText(self, height="8",width="60",)
+        self.scr_notes.grid(row="3", column="0", columnspan="3")
+        #------------------------------------------
+        self.btn_cancel = tk.Button(self, text="Cancel")
+        self.btn_cancel.grid(row="4", column="0")
+        self.btn_cancel = tk.Button(self, text="Submit")
+        self.btn_cancel.grid(row="4", column="2")    
+        #-------------------------------------------
+        self.grid_columnconfigure( 0, weight="1")
+        self.grid_columnconfigure( 1, weight="1")
+        self.grid_columnconfigure( 2, weight="1")
+class MarkedFrame(tk.Frame):
+    def __init__(self):
+        tk.Frame.__init__(self)
+        self.lbl_notes = tk.Label(self, text="These items are marked for removal ").grid(row="0", column="0")
+        self.scr_items = scrolledtext.ScrolledText(self, height="8",width="60",)
 if __name__ == "__main__":
     datafile = open("game_lib.pickle" , "rb")
     library_database = pickle.load(datafile)
