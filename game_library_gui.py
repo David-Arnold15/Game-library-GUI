ec682085800cd5fa16c2f2f5f82be813122b43f0
David-Arnold15 <you@example.com>
2020-03-02 13:39:28 -0600

    modified:   game_library_gui.py

--- game_library_gui.py
+++ game_library_gui.py
@@ -1,6 +1,7 @@
 import pickle
 import tkinter as tk
 from tkinter import scrolledtext
+from tkinter import messagebox as mb
 TITLE_FONT=('Times New Roman', 24)
 BUTTON_FONT = ('Times New Roman', 24)
 class Screen(tk.Frame):
@@ -43,6 +44,8 @@ class MainMenu(Screen):
         self.grid_rowconfigure(5, weight = 3)
     def go_add(self):
         Screen.current= 4
+        screens[Screen.current].clear()
+        screens[Screen.current].edit_key = len(library_database.keys())+1
         Screen.switch_frame()    
     def go_edit(self):
         pop_up = tk.Tk()
@@ -158,55 +161,78 @@ class InfoFrameEntries(tk.Frame):
 
     def __init__(self,parent):
         tk.Frame.__init__(self, master= parent)    
+        
         #--------------------------------------
+        
         self.lbl_genre = tk.Label(self, text="Genre").grid(row="0", column="0")
         self.ent_genre = tk.Entry(self, text="Genre")
         self.ent_genre.grid(row="0", column="1")
+        
         #---------------------------------------
+        
         self.lbl_title = tk.Label(self, text="Title").grid(row="0", column="2")
         self.ent_title = tk.Entry(self, text="Title")
         self.ent_title.grid(row="0", column="3")
+        
         #---------------------------------------
+        
         self.lbl_dev = tk.Label(self, text="Developer").grid(row="1", column="0")
         self.ent_dev = tk.Entry(self, text="Developer")
         self.ent_dev.grid(row="1", column="1")    
+        
         #---------------------------------------
+        
         self.lbl_pub = tk.Label(self, text="Publisher").grid(row="1", column="2")
         self.ent_pub = tk.Entry(self, text="Publisher")
         self.ent_pub.grid(row="1", column="3") 
+        
         #---------------------------------------
+        
         self.lbl_system = tk.Label(self, text="System").grid(row="2", column="0")
         self.ent_system = tk.Entry(self, text="System")
         self.ent_system.grid(row="2", column="1")   
+        
         #---------------------------------------
+        
         self.lbl_release_date = tk.Label(self, text="Release Date").grid(row="2", column="2")
         self.ent_release_date = tk.Entry(self, text="Release Date")
         self.ent_release_date.grid(row="2", column="3")
+        
         #---------------------------------------
+        
         self.lbl_price = tk.Label(self, text="Price").grid(row="3", column="0")
         self.ent_price = tk.Entry(self, text="Price")
         self.ent_price.grid(row="3", column="1")
+        
         #---------------------------------------
+        
         self.lbl_purchase_date = tk.Label(self, text="Purchase_date").grid(row="3", column="2")
         self.ent_purchase_date = tk.Entry(self, text="Purchase Date")
         self.ent_purchase_date.grid(row="3", column="3")      
+        
         #--------------------------------------
+        
         self.lbl_rating = tk.Label(self, text="Rating").grid(row="4", column="0")
         self.ent_rating = tk.Entry(self, text="Rating")
         self.ent_rating.grid(row="4", column="1")      
+        
         #---------------------------------------
 """-------------------------------------------------------------------------------------------------------------------------------------"""
+
 class InfoFrame(Screen):
     def __init__(self,):
         Screen.__init__(self)
         self.edit_key = 0
+        
         #-----------------------------------------
+        
         self.info_frame_entries = InfoFrameEntries(self)
         self.info_frame_entries.grid(row="0", column="0" ,columnspan="3", sticky="news")
         var1=None
         chk_beat = tk.Checkbutton(self, text='Beat it?',variable=var1, onvalue=1, offvalue=0,)
         
         chk_beat.grid(row="1", column="0")        
+        
         #---------------------------------------
         
         options=["Single or Multiplayer?", "Single", "Multi"]
@@ -214,17 +240,24 @@ class InfoFrame(Screen):
         tkvar.set(options[0])
         self.menu = tk.OptionMenu(self, tkvar, *options)
         self.menu.grid(row="1", column="2", )
+        
         #------------------------------------------
         
         self.lbl_notes = tk.Label(self, text="Notes: ").grid(row="2", column="0", sticky="w" )
         self.scr_notes = scrolledtext.ScrolledText(self, height="8",width="60",)
         self.scr_notes.grid(row="3", column="0", columnspan="3")
+        
         #------------------------------------------
         
         self.btn_cancel = tk.Button(self, text="Cancel", command=self.go_menu)
         self.btn_cancel.grid(row="4", column="0")
-        self.btn_submit = tk.Button(self, text="Submit", command=self.go_menu )
+        
+        self.btn_reset = tk.Button(self, text="Reset", command=self.clear)
+        self.btn_reset.grid(row="4", column="1")
+        
+        self.btn_submit = tk.Button(self, text="Submit", command=self.done )
         self.btn_submit.grid(row="4", column="2")    
+        
         #-------------------------------------------
         
         self.grid_columnconfigure( 0, weight="1")
@@ -234,8 +267,35 @@ class InfoFrame(Screen):
         print("go_menu")
         Screen.current= 0
         Screen.switch_frame()
+    def done(self):
+        entry = []
+        
+        #----------------------------------------------------
+        
+        entry.append(self.info_frame_entries.ent_genre.get())
+        entry.append(self.info_frame_entries.ent_title.get())
+        entry.append(self.info_frame_entries.ent_dev.get())
+        entry.append(self.info_frame_entries.ent_pub.get())
+        entry.append(self.info_frame_entries.ent_system.get())
+        entry.append(self.info_frame_entries.ent_rating.get())
+        entry.append(self.info_frame_entries.ent_release_date.get())
+        entry.append("single")
+        entry.append(self.info_frame_entries.ent_price.get())
+        entry.append("yes")
+        entry.append(self.info_frame_entries.ent_purchase_date.get())
+        entry.append(self.scr_notes.get(0.0, "end"))
+        
+        
+        #----------------------------------------------------
+        
+        library_database[self.edit_key] = entry
+       
+        mb.showinfo(message = "Entry has been added")
+        
+        Screen.current= 0
+        Screen.switch_frame()        
+    
     def update(self):
-        #['price', 'completion status', 'purchase date', 'notes']
         entry = library_database[self.edit_key]
         self.info_frame_entries.ent_genre.delete(0,"end")
         self.info_frame_entries.ent_genre.insert(0, entry[0])
@@ -245,7 +305,7 @@ class InfoFrame(Screen):
         
         self.info_frame_entries.ent_dev.delete(0,"end")
         self.info_frame_entries.ent_dev.insert(0, entry[2])
-        
+     
         self.info_frame_entries.ent_pub.delete(0,"end")
         self.info_frame_entries.ent_pub.insert(0, entry[3])
         
@@ -264,6 +324,21 @@ class InfoFrame(Screen):
         self.info_frame_entries.ent_purchase_date.delete(0,"end")
         self.info_frame_entries.ent_purchase_date.insert(0, entry[10])
         
+        self.scr_notes.delete(0.0, "end")
+        self.scr_notes.insert(0.0, entry[11])
+    
+    def clear(self):
+        self.info_frame_entries.ent_genre.delete(0,"end")
+        self.info_frame_entries.ent_title.delete(0,"end")
+        self.info_frame_entries.ent_dev.delete(0,"end")
+        self.info_frame_entries.ent_pub.delete(0,"end")
+        self.info_frame_entries.ent_system.delete(0,"end")
+        self.info_frame_entries.ent_rating.delete(0,"end")
+        self.info_frame_entries.ent_release_date.delete(0,"end")
+        self.info_frame_entries.ent_price.delete(0,"end")
+        self.info_frame_entries.ent_purchase_date.delete(0,"end")
+        self.scr_notes.delete(0.0, "end")
+
 class MarkedFrame(Screen):
     def __init__(self):
         Screen.__init__(self)
@@ -271,26 +346,60 @@ class MarkedFrame(Screen):
         self.scr_items = scrolledtext.ScrolledText(self, height="8",width="60", )
         self.scr_items.grid(row="1", column="0", columnspan="3")
         #------------------------------------------
+        
         self.btn_cancel = tk.Button(self, text="Cancel", command=self.go_menu)
         self.btn_cancel.grid(row="2", column="0")
         self.btn_submit = tk.Button(self, text="Submit", command=self.go_menu)
         self.btn_submit.grid(row="2", column="2")   
     def go_menu(self):   
         Screen.current= 0
-        Screen.switch_frame()     
+        Screen.switch_frame()
+        
 class PrintFrameChecks(tk.Frame):
-    def __init__(self,parent):
-        tk.Frame.__init__(self, master= parent)
-        ['Developer', 'Publisher', 'System', 'Rating' , 'release date', 'single/multi?', 'price', 'completion status', 'purchase date', 'notes']
-        chk_genre = tk.Checkbutton(self, text='Genre',variable=var1, onvalue=1, offvalue=0,)
-        
-        chk_title = tk.Checkbutton(self, text='Title',variable=var1, onvalue=1, offvalue=0,)
-        
 
+    def __init__(self, parent):
+        tk.Frame.__init__(self, master=parent)
+        var1 = None
+        self.chk_genre = tk.Checkbutton(self, text='Genre',variable=var1, onvalue=1, offvalue=0,)
+        self.chk_genre.grid(row = 0, column=0, sticky="news")
+        
+        self.chk_title = tk.Checkbutton(self, text='Title',variable=var1, onvalue=1, offvalue=0,)
+        self.chk_title.grid(row = 0, column=1, sticky="news")
+        
+        self.chk_dev = tk.Checkbutton(self, text='Developer',variable=var1, onvalue=1, offvalue=0,)
+        self.chk_dev.grid(row = 0, column=2, sticky="news")
+        
+        self.chk_pub = tk.Checkbutton(self, text='Publisher',variable=var1, onvalue=1, offvalue=0,)
+        self.chk_pub.grid(row = 1, column=0, sticky="news")
+
+        self.chk_system = tk.Checkbutton(self, text='System',variable=var1, onvalue=1, offvalue=0,)
+        self.chk_system.grid(row = 1 , column = 1, sticky="news")
+        
+        self.chk_rating = tk.Checkbutton(self, text='Rating',variable=var1, onvalue=1, offvalue=0,)
+        self.chk_rating.grid(row = 1, column=2, sticky="news")
+        
+        self.chk_release = tk.Checkbutton(self, text='Release date',variable=var1, onvalue=1, offvalue=0,)
+        self.chk_release.grid(row = 2, column=0, sticky="news")
+        
+        self.chk_players = tk.Checkbutton(self, text='single/multi',variable=var1, onvalue=1, offvalue=0,)
+        self.chk_players.grid(row = 2, column=1, sticky="news")
+        
+        self.chk_price = tk.Checkbutton(self, text='price',variable=var1, onvalue=1, offvalue=0,)
+        self.chk_price.grid(row = 2, column=2, sticky="news")
+        
+        self.chk_beat = tk.Checkbutton(self, text='Beat it',variable=var1, onvalue=1, offvalue=0,)
+        self.chk_beat.grid(row = 3, column=0, sticky="news")
+        
+        self.chk_purchase_date = tk.Checkbutton(self, text='Purchase Date',variable=var1, onvalue=1, offvalue=0,)
+        self.chk_purchase_date.grid(row = 3, column=1, sticky="news")
 class PrintFrame(Screen):
     def __init__(self):
         Screen.__init__(self)
+        
+           
+        
         #-----------------------------------------
+        
         self.lbl_search = tk.Label(self, text="Search", font=TITLE_FONT).grid(row='0', column='0', columnspan='3')
         tk.Frame.__init__(self)
         self.lbl_search_by = tk.Label(self, text="Search By").grid(row="1", column="0")
@@ -298,21 +407,30 @@ class PrintFrame(Screen):
         tkvar = tk.StringVar(self)
         tkvar.set(options[0])
         self.menu_search_by = tk.OptionMenu(self, tkvar, *options)
-        self.menu_search_by.grid(row="2", column="0", columnspan="2")        
+        self.menu_search_by.grid(row="2", column="0", )        
+        
         #------------------------------------------
+        
         self.lbl_search_for = tk.Label(self, text="Search For: ").grid(row="3", column="0")
         self.ent_search_for = tk.Entry(self,)
         self.ent_search_for.grid(row="4", column="0")
+        
         #------------------------------------------
+        
         self.scr_info = scrolledtext.ScrolledText(self, height="8",width="60")
         self.scr_info.grid(row="5", column="0", columnspan='3')
+        
         #------------------------------------------
+        
         self.btn_cancel = tk.Button(self, text="Cancel",command=self.go_menu)
         self.btn_cancel.grid(row="6", column="0")
         self.btn_reset = tk.Button(self, text="Reset", )
         self.btn_reset.grid(row="6", column="1")
         self.btn_confirm = tk.Button(self, text="Submit")
         self.btn_confirm.grid(row="6", column="2")
+    
+        self.frm_print_frame_checks = PrintFrameChecks(self)
+        self.frm_print_frame_checks.grid(row=2, column=1, rowspan=3, columnspan=3 ,sticky="news")
     def go_menu(self):   
         print("go_menu")
         Screen.current= 0
@@ -330,7 +448,7 @@ if __name__ == "__main__":
     root.grid_rowconfigure(0, weight="1")
     root.geometry("500x500")
     screens = [MainMenu(), None, None,None,InfoFrame(),MarkedFrame(),
-               PrintFrame()]    
+               PrintFrame(),]    
     for i in range(len(screens)):
         try:
             screens[i].grid(row="0", column="0", sticky="news")
